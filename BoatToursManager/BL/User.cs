def7a26c393f4fd78d974f14f7bf4483ced9b617
kiran.karki <kiran.karki@student.fh-kiel.de>
2020-03-20 12:30:46 +0100

    user branch

---
+++ BoatToursManager/BL/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoatToursManager.BL
+{
+    public class User
+    {
+        public int id { get; private set; } = 0;
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+        public UserType userType { get; set; }
+        public Address userAddress { get; set; }
+        public Guid activationCode { get; set; } = Guid.NewGuid();
+        public bool isEmailVerified { get; set; }
+        public Guid passwordResetCode { get; set; }
+
+       
+    }
+}
---
+++ BoatToursManager/Models/UserLoginModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoatToursManager.Models
+{
+    public class UserLoginModel
+    {
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Please enter Email ID")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
+        public string email { get; set; }
+        [Required]
+        [StringLength(18, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        //[RegularExpression(@"^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)).+$")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string password { get; set; }
+        [Display(Name = "Remember Me?")]
+        public bool rememberMe { get; set; }
+        public static int getLoggedInUser()
+        {
+            int id = (int)System.Web.HttpContext.Current.Session["userID"];
+            return id;
+        }
+    }
+}
